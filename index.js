25f363ae63ad385fc62806d5092fd10df5dd279c
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-13 19:25:34 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+// const cors = require("cors");
+require("./db/config");
+const User = require("./db/user");
+const app = express();
+
+app.use(express.json());
+// app.use(cors());
+
+app.post("/register", async (req, resp) => {
+  let user = await User(req.body);
+  let result = await user.save();
+  resp.send(result);
+});
+app.listen(5173);
