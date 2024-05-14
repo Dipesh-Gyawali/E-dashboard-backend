01ecb430b648150302e515f7be84559a66c89a5e
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-14 18:24:50 +0545

    feat: cors added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 require("./db/config");
-const User = require("./db/user");
+const User = require("./db/User");
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.post("/register", async (req, resp) => {
   let user = await User(req.body);
   let result = await user.save();
   resp.send(result);
 });
-app.listen(5173);
+app.listen(5000);
