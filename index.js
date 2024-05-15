f2edd76349281aa33bd2fa8ba09d0b14275cb465
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-15 17:48:22 +0545

    feat: login API

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(cors());
 app.post("/register", async (req, resp) => {
   let user = await User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    resp.send(user);
+  } else {
+    resp.send({ result: "No User found" });
+  }
 });
 app.listen(5000);
