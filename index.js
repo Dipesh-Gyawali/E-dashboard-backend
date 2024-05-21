248abe8529dc5323f3b1f27da13bcba324198d88
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-05-21 17:04:22 +0545

    feat: api for get single product

--- index.js
+++ index.js
@@ -41,4 +41,13 @@ app.delete("/product/:id", async (req, resp) => {
   let result = await Product.deleteOne({ _id: req.params.id });
   resp.send(result);
 }),
-  app.listen(5000);
+  app.get("/product/:id", async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+      resp.send(result);
+    } else {
+      resp.send({ result: "No Record Found." });
+    }
+  });
+
+app.listen(5000);
