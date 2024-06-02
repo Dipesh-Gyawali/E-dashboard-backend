9f32f1b562135c01e8076b0e2856e751cd6f4f10
Dipesh Gyawali <112164101+Dipesh-Gyawali@users.noreply.github.com>
2024-06-02 06:34:08 +0545

    feat: jwt token added on register and login

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const cors = require("cors");
 require("./db/config");
 const User = require("./db/User");
 const Product = require("./db/Product");
+
 const app = express();
 
+const Jwt = require("jsonwebtoken");
+const jwtKey = "e-com";
+
+// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors());
 
@@ -13,21 +18,39 @@ app.post("/register", async (req, resp) => {
   let result = await user.save();
   result = result.toObject();
   delete result.password;
-  resp.send(result);
+
+  Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      resp.send("Something went wrong");
+    }
+    resp.send({ result, auth: token });
+  });
 });
+
 app.post("/login", async (req, resp) => {
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
-    resp.send(user);
+    if (user) {
+      Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          resp.send("Something went wrong, please try after sometime");
+        }
+        resp.send({ user, auth: token });
+      });
+    } else {
+      resp.send({ result: "No User found" });
+    }
   } else {
     resp.send({ result: "No User found" });
   }
 });
+
 app.post("/add-product", async (req, resp) => {
   let product = await Product(req.body);
   let result = await product.save();
   resp.send(result);
 });
+
 app.get("/products", async (req, resp) => {
   const products = await Product.find();
   if (products.length > 0) {
@@ -49,6 +72,7 @@ app.delete("/product/:id", async (req, resp) => {
       resp.send({ result: "No Record Found." });
     }
   });
+
 app.put("/product/:id", async (req, resp) => {
   let result = await Product.updateOne(
     { _id: req.params.id },
@@ -56,6 +80,7 @@ app.put("/product/:id", async (req, resp) => {
   );
   resp.send(result);
 });
+
 app.get("/search/:key", async (req, resp) => {
   let result = await Product.find({
     $or: [
